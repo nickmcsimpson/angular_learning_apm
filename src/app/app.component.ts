078a7345bed6456b356a0ae4175a1d48e2cad8a7
Nicholas McClain-Simpson <n.mcsimp@gmail.com>
2020-12-17 14:29:51 -0600

    Initialize AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 // Decorator
 @Component({
   // Metadata
   selector: 'pm-root',
-  // View layout with file:
+    // View layout with file:
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-  // View Layout here
-  /*template `
+    // View Layout here
+  /*template: `
   * <div>HTML</div>
   */
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  // title = 'Angular: Getting Started';
   // Property in TypeScript
   pageTitle: string = 'Acme Product Management'
 }
+
+//Index.html holds all the scripts for launching the angular app
+  //often the only 'Web Page' of the application hence SPA
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Model Decorator
 @NgModule({
+  // List of components
   declarations: [
     AppComponent
   ],
+  // External Modules
   imports: [
     BrowserModule
   ],
+  // Startup component (contains selector defined in index.html)
   bootstrap: [AppComponent]
 })
 export class AppModule { }
