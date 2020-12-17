080b2e5f20b396c6b51af7e96f69864df3be9512
Nicholas McClain-Simpson <n.mcsimp@gmail.com>
2020-12-17 16:23:04 -0600

    Two Way Binding for filter input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+// Imports needed for included components are defined here
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component'
@@ -13,7 +15,8 @@ import { ProductListComponent } from './products/product-list.component'
   ],
   // External Modules
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   // Startup component (contains selector defined in index.html)
   bootstrap: [AppComponent]
