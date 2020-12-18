2470efc0b7e9d5e3314198558bd91180447c7387
Nicholas McClain-Simpson <n.mcsimp@gmail.com>
2020-12-18 11:17:04 -0600

    Add Products Service Dependency Injection

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { IProduct } from './product'
+import { ProductService } from './product.service'
 
 @Component({
     selector: 'pm-products',
@@ -27,32 +28,19 @@ export class ProductListComponent implements OnInit {
 
     
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2019",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2019",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "assets/images/garden_cart.png"
-        }
-    ];
+    products: IProduct[];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    // Dependency Injection: Product service (verbose)
+    // private _productService
+    // constructor(productService: ProductService) {
+    //     this._productService = productService;
+
+        // Sugar:
+    constructor(private productService: ProductService) {
+        // Don't call service here, keep constructor light
+            // Instead use the onInit lifecycle method
+        // this.filteredProducts = this.products;
+        // this.listFilter = 'cart';
     }
 
     // Event Handling
@@ -68,6 +56,9 @@ export class ProductListComponent implements OnInit {
 
     // Interface method 
     ngOnInit(): void {
+        // Call service to get products list
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
         console.log('In OnInit');
     }
 
