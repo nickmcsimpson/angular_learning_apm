3eb1193fa72185c93fbce7eb8ab2333665b63382
Nicholas McClain-Simpson <n.mcsimp@gmail.com>
2020-12-18 09:55:51 -0600

    Interface Product, Filtering and Custom Pipe

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,17 +1,33 @@
-import {Component} from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { IProduct } from './product'
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',//relative path
+    // styles: ['thead {color: #337AB7;}']//Inline Styles
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     // TypeScript can infer type from these pre-defined variables
     pageTitle: string = 'Product List!';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] = [
+    // listFilter: string = 'cart';
+
+    // Use Getter and Setter instead
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
         {
           "productId": 1,
           "productName": "Leaf Rake",
@@ -34,9 +50,25 @@ export class ProductListComponent {
         }
     ];
 
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+
     // No return type or keywords for function
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
     // Change detection will automatically reevaluate the bound displays
+
+    // Interface method 
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => 
+                product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
 }
